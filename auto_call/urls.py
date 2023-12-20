889de1c9701c3e852bd4e8aeb34def5e2c772c58
khaledrh <k_ragab@rocketmail.com>
2023-12-20 17:51:35 +0200

    Changes to be committed: 	modified:   auto_call/__pycache__/form.cpython-311.pyc 	modified:   auto_call/__pycache__/urls.cpython-311.pyc 	modified:   auto_call/__pycache__/views.cpython-311.pyc 	modified:   auto_call/form.py 	modified:   auto_call/static/auto_call/style.css 	modified:   auto_call/templates/auto_call/patient_form.html 	modified:   auto_call/templates/auto_call/patient_list.html 	modified:   auto_call/urls.py 	modified:   auto_call/views.py 	modified:   db.sqlite3

--- auto_call/urls.py
+++ auto_call/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from auto_call import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
-path("<int:roomid>/", views.HomeListView.as_view() , name="home"),
-path("room/<int:roomid>/", views.PatientListView.as_view(), name="roomlist"),
-path("form/", views.patient_form, name="form"),
-path("screen/", views.screen, name="screen"),
-]
+    path("", RedirectView.as_view(url="http://127.0.0.1:8000/0/")),
+    path("<int:roomid>/", views.HomeListView.as_view() , name="home"),
+    path("room/<int:roomid>/", views.PatientListView.as_view(), name="roomlist"),
+    path("form/", views.patient_form, name="form"),
+    path("delete/<int:roomid>", views.patient_form_del, name="formdel"),
+    path("screen/", views.screen, name="screen"),
+    ]
--- auto_call/views.py
+++ auto_call/views.py
@@ -2,9 +2,8 @@ from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-from auto_call.models import Patient
+from auto_call.models import Patient, Room
 from auto_call.form import PatientForm
-from auto_call.form import RoomForm
 from django.utils.timezone import datetime
 from django.shortcuts import redirect
 
@@ -17,9 +16,9 @@ class HomeListView(ListView):
     def get_queryset(self):
         room = self.kwargs["roomid"]
         if room == 0:
-            return Patient.objects.order_by("log_date")
+            return Patient.objects.order_by("-emergancy","log_date")
         else:
-            return Patient.objects.order_by("log_date").filter(room = self.kwargs["roomid"])
+            return Patient.objects.order_by("-emergancy","log_date").filter(room = self.kwargs["roomid"])
         
 
 class PatientListView(ListView):
@@ -28,21 +27,40 @@ class PatientListView(ListView):
     template_name="auto_call/patient_in_waiting.html"
     
     def get_queryset(self):
-        return Patient.objects.order_by("log_date").filter(room = self.kwargs["roomid"])[:5]
+        return Patient.objects.order_by("-emergancy","log_date").filter(room = self.kwargs["roomid"])[:5]
 
 
 def screen(request):
     return render(request, "auto_call/screen.html")
 
-
 def patient_form(request):
     form = PatientForm(request.POST or None)
+    rooms = Room.objects.all()
 
     if request.method == "POST":
         if form.is_valid():
             message = form.save(commit=False)
+            # message.room = 
+            room_id = request.POST.get('room_id')
+            # room = Room.objects.get(pk=room_id)
+            # message.room = room
             message.log_date = datetime.now()
             message.save()
             return redirect("form")
     else:
-        return render(request, "auto_call/patient_form.html", {"form": form})
+        return render(request, "auto_call/patient_form.html", {"form": form , "rooms": rooms})
+    
+
+def patient_form_del(request, roomid):
+    if request.method == "POST":
+        # Retrieve the patient based on roomid
+        patient = get_object_or_404(Patient, room=roomid)
+        
+        # Delete the patient
+        patient.delete()
+        
+        # Redirect to a suitable URL after deletion (You can change this as per your requirement)
+        return redirect("home")
+    else:
+        # Handle the case where the user accesses the URL without a POST request
+        return "Bad Request"
