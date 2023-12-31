ce66bc2d528f2339ce3f44a275a2cc54f0912f5f
khaledrh <k_ragab@rocketmail.com>
2023-12-31 12:35:34 +0200

    Changes to be committed: 	modified:   auto_call/__pycache__/urls.cpython-311.pyc 	deleted:    auto_call/static/auto_call/slideshow/1.PNG 	deleted:    auto_call/static/auto_call/slideshow/2.png 	deleted:    auto_call/static/auto_call/slideshow/3.mp4 	new file:   auto_call/static/auto_call/slideshow/VID-20231231-WA0000.mp4 	new file:   auto_call/static/auto_call/slideshow/VID-20231231-WA0001.mp4 	modified:   auto_call/urls.py 	new file:   run_server.bat

--- auto_call/urls.py
+++ auto_call/urls.py
@@ -3,7 +3,7 @@ from auto_call import views
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("", RedirectView.as_view(url="http://127.0.0.1:8000/0/")),
+    path("", RedirectView.as_view(url="/0/")),
     path("<int:roomid>/", views.HomeListView.as_view() , name="home"),
     path("room/<int:roomid>/", views.PatientListView.as_view(), name="roomlist"),
     path("form/", views.patient_form, name="form"),
